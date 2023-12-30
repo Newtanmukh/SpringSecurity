e97a6f79ce45239deb6f828636152e679a397e99
Newtanmukh <importantnewton@gmail.com>
2023-12-30 23:24:08 +0530

    JWT Filters and SecurityConfig.java added

---
+++ spring-boot-security-learn/src/main/java/com/example/Security/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.example.Security.config;
+
+import com.example.Security.security.JwtAuthenticationEntryPoint;
+import com.example.Security.security.jwtAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+public class SecurityConfig {
+
+
+    @Autowired
+    private JwtAuthenticationEntryPoint point;
+    @Autowired
+    private jwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(csrf -> csrf.disable())
+                .cors(AbstractHttpConfigurer::disable)
+                .authorizeRequests().
+                requestMatchers("/test").authenticated().requestMatchers("/auth/login").permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .exceptionHandling(ex -> ex.authenticationEntryPoint(point))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));//STATELESS, means dont store anything in server.
+
+        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+
+
+}
