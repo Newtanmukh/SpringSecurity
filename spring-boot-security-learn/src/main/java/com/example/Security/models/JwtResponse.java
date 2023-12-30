48ba4f0b476fc687e7af9d81a37bc64889d117f7
Newtanmukh <importantnewton@gmail.com>
2023-12-30 23:36:22 +0530

    Annotated the JwtResponse.java

--- spring-boot-security-learn/src/main/java/com/example/Security/models/JwtResponse.java
+++ spring-boot-security-learn/src/main/java/com/example/Security/models/JwtResponse.java
@@ -1,7 +1,15 @@
 package com.example.Security.models;
 
-public class JwtResponse {
 
+import lombok.*;
+
+@Builder
+@ToString
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class JwtResponse {
     private String jwtToken;
     private String userName;
 }
