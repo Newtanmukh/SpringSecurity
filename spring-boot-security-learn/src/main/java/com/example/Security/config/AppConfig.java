ccb3676c15b5da55080006c58ac87ecc38a556bc
Newtanmukh <importantnewton@gmail.com>
2023-12-30 21:59:29 +0530

    Added JWT Dependencies

---
+++ spring-boot-security-learn/src/main/java/com/example/Security/config/AppConfig.java
@@ -0,0 +1,41 @@
+package com.example.Security.config;
+
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class AppConfig {
+
+
+    @Bean
+    public UserDetailsService userDetailsService(@Qualifier("decryptPasswordEncoder") PasswordEncoder passwordEncoder){
+
+         UserDetails user = User.builder().
+                            username("newtan").
+                            password(passwordEncoder.encode("abc")).
+                            roles("ADMIN").
+                            build();
+
+        UserDetails user1 = User.builder().
+                            username("newton").
+                            password(passwordEncoder.encode("123")).
+                            roles("ADMIN").
+                            build();
+
+         return new InMemoryUserDetailsManager(user ,user1);
+    }
+
+    @Bean(name = "decryptPasswordEncoder")
+    public PasswordEncoder decryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+}
--- spring-boot-security-learn/src/main/java/com/example/Security/controller/UserController.java
+++ spring-boot-security-learn/src/main/java/com/example/Security/controller/UserController.java
@@ -1,35 +1,35 @@
 package com.example.Security.controller;
 
 
-import com.example.Security.models.User;
 import com.example.Security.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.List;
+
+
+//http://localhost:8080/users/currentUser
+
 
 @RestController  //basically means that data returned will be JSON  data.
 @RequestMapping("/users")
 public class UserController {
 
     @Autowired
-    private UserService userService;
+    private UserDetailsService userService;
 
     //all users
-    @GetMapping("/")
-    public List<User> getAllUsers(){
-        return this.userService.getUsers();
+//    @GetMapping("/user")
+//    public List<User> getAllUsers(){
+//        return userService.getUsers();
+//    }
+    @GetMapping("/currentUser")
+    public String getCurrentUser(Principal principal){
+        return principal.getName();
     }
 
-    //return single user
-    @GetMapping("/{username}")
-    public User getUser(@PathVariable("username") String username){
-        return this.userService.getUser(username);
-    }
-
-    @GetMapping("/{username}")
-    public User addUser(@RequestBody User user){
-        return this.userService.addUser(user);
-    }
 
  }
