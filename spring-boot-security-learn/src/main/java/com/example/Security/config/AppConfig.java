33f27eb9870bc503c8cc29b4589aa32b964daf3d
Newtanmukh <importantnewton@gmail.com>
2023-12-30 23:38:40 +0530

    created the AuthenticationManager.

--- spring-boot-security-learn/src/main/java/com/example/Security/config/AppConfig.java
+++ spring-boot-security-learn/src/main/java/com/example/Security/config/AppConfig.java
@@ -4,6 +4,8 @@ package com.example.Security.config;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -36,6 +38,11 @@ public class AppConfig {
     @Bean(name = "decryptPasswordEncoder")
     public PasswordEncoder decryptPasswordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration builder) throws Exception {
+        return builder.getAuthenticationManager();
     }
 
 }
